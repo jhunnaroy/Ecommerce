df9bc9d850f24f09d67a8484793be8874b2a252f
Jhunna Kumar <jhunnaroy@gmail.com>
2026-05-15 16:57:32 +0530

    efg

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -11,11 +11,11 @@ const router = express.Router();
 
 const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 
-router.route("/order/new").post(isAuthenticatedUser, newOrder);
+router.route("/order/new").post( newOrder);
 
-router.route("/order/:id").get(isAuthenticatedUser, getSingleOrder);
+router.route("/order/:id").get( getSingleOrder);
 
-router.route("/orders/me").get(isAuthenticatedUser, myOrders);
+router.route("/orders/me").get(myOrders);
 
 router
   .route("/admin/orders")
